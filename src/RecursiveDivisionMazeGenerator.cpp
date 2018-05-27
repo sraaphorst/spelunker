08403852c8224bfdbd6e8c574916bd2795a36094
Sebastian Raaphorst <sraaphor@gemini.edu>
2018-05-27 11:11:13 -0400

    T8: Fixed the recursive division algorithm and simplified. (#42)

--- src/RecursiveDivisionMazeGenerator.cpp
+++ src/RecursiveDivisionMazeGenerator.cpp
@@ -13,14 +13,12 @@
 #include "RNG.h"
 #include "RecursiveDivisionMazeGenerator.h"
 
-namespace spelunker::maze {
-    RecursiveDivisionMazeGenerator::RecursiveDivisionMazeGenerator(int w, int h,
-                                                                   double lb, double ub,
-                                                                   double p, bool alt)
-            : MazeGenerator(w, h), lowerBound(lb), upperBound(ub), verticalProbability(p), alternate(alt) {}
+#include <iostream>
+using namespace std;
 
+namespace spelunker::maze {
     RecursiveDivisionMazeGenerator::RecursiveDivisionMazeGenerator(int w, int h)
-            : RecursiveDivisionMazeGenerator(w, h, 0.0, 1.0, 0.5, false) {}
+            : MazeGenerator(w, h) {}
 
     const Maze RecursiveDivisionMazeGenerator::generate() {
         // Unlike other algorithms, we start with no walls, and then add them iteratively.
@@ -29,10 +27,6 @@ namespace spelunker::maze {
         // Now create the container of rectangles to solve, and go through iteratively.
         std::queue<rectangle> areas;
         areas.push(rectangle(0, 0, width, height));
-
-        // We want to start by dividing up the greater dimension, so if width > height,
-        // we want to divide width first: thus, set preVertical to what we don't wan't.
-        auto prevVertical = width < height;
 
         while (!areas.empty()) {
             const auto area = areas.front();
@@ -45,18 +39,14 @@ namespace spelunker::maze {
             // First decide if we are splitting this area horizontally or vertically.
             // If the height is 1, we must split vertically.
             // Otherwise, if the width is 1, we must split horizontally.
-            // Otherwise, we can split either way.
-            const auto vertical = (area.h == 1)
-                    || (area.w > 1
-                        && ((alternate && !prevVertical) || (!alternate && math::RNG::randomProbability() < verticalProbability)));
-            prevVertical = vertical;
+            // Otherwise, we split in the direction that will result in the closest w:h ratio.
+            const auto vertical = (area.h == 1) || (area.w > area.h);
 
             // Pick a valid cell in the range. If lowerBound = 0 and upperBound = 1,
             // then we get any cell in the range, but we want to omit the last row
             // or column since all walls will be E or S.
-            const auto lowerCell = (int) (lowerBound * (vertical ? area.w : area.h));
-            const auto upperCell = (int) (upperBound * (vertical ? area.w : area.h));
-            const auto pos = (lowerCell >= upperCell) ? lowerCell : math::RNG::randomRange(lowerCell, std::min((vertical ? area.w : area.h) - 1, upperCell));
+            const auto upperCell = vertical ? area.w : area.h;
+            const auto pos = math::RNG::randomRange(upperCell-1);
 
             // Now we need a random gap in the wall.
             const auto gap = math::RNG::randomRange(vertical ? area.h : area.w);
@@ -72,7 +62,7 @@ namespace spelunker::maze {
                 else
                     wi[rankPos(types::pos(area.x + i, area.y + pos, types::SOUTH))] = true;
             }
-
+            
             // Now split into two areas. Since we are always adding walls on the E and S,
             // we must do this carefully.
             if (vertical) {
