aaf3d7d434ff701165a3e4a071f2f7cc8d39f12e
sraaphorst <sraaphorst@gemini.edu>
2018-05-31 02:28:35 -0400

    T5: Files reorganized and initial cmake scripts written.

---
+++ src/math/DefaultRNG.h
@@ -0,0 +1,39 @@
+/**
+ * DefaultRNG.h
+ *
+ * By Sebastian Raaphorst, 2018.
+ *
+ * The default random number generator using a Mersenne twister
+ */
+
+#ifndef SPELUNKER_DEFAULTRNG_H
+#define SPELUNKER_DEFAULTRNG_H
+
+#include <random>
+
+#include "RNG.h"
+
+namespace spelunker::math {
+    /// The default random number generator, which uses STL's implementation of the Mersenne twister.
+    /**
+     * The default random number generator, using STL's implementation of the Mersenne twister.
+     * If no other RNG is set, this is used by default when accessing the RNG class.
+     * No action is required on the part of the user to initialize it.
+     */
+    class DefaultRNG final : public RNG {
+    public:
+        DefaultRNG();
+        ~DefaultRNG() final = default;
+
+    protected:
+        int randomRangeImpl(int lower, int upper) noexcept final;
+
+        double randomProbabilityImpl() noexcept final;
+
+    private:
+        std::random_device rd;
+        std::mt19937 g;
+    };
+};
+
+#endif //SPELUNKER_DEFAULTRNG_H
