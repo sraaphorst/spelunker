b5b49475d626db4600b042d51a1da808d2ea8533
sraaphorst <sraaphorst@gemini.edu>
2018-06-14 21:49:06 -0400

    T81: Made some composable homomorphisms and types.

--- apps/test_homomorphisms.cpp
+++ apps/test_homomorphisms.cpp
@@ -17,26 +17,32 @@
 #include "graphmaze/GraphMazeTypeclasses.h"
 #include "thickmaze/ThickMaze.h"
 #include "thickmaze/ThickMazeTypeclasses.h"
+using namespace spelunker;
+using namespace typeclasses;
+
+// This declares the type to be available everywhere.
+template <> class Homomorphism<graphmaze::GraphMaze, thickmaze::ThickMaze> :
+        public CompositionMorphism<Homomorphism<graphmaze::GraphMaze, maze::Maze>,
+                Homomorphism<maze::Maze, thickmaze::ThickMaze>> {};
 
 int main(int argc, char *argv[]) {
-    spelunker::maze::DFSMazeGenerator gen(50,40);
-    spelunker::maze::Maze m = gen.generate();
-    std::cout << spelunker::typeclasses::Show<spelunker::maze::Maze>::show(m);
+    maze::DFSMazeGenerator gen(50,40);
+    maze::Maze m = gen.generate();
+    std::cout << Show<spelunker::maze::Maze>::show(m);
     std::cout << std::endl;
 
-    spelunker::graphmaze::GraphMaze gm = spelunker::typeclasses::Homomorphism<spelunker::maze::Maze, spelunker::graphmaze::GraphMaze>::morph(m);
-    std::cout << spelunker::typeclasses::Show<spelunker::graphmaze::GraphMaze>::show(gm);
+    graphmaze::GraphMaze gm = Homomorphism<maze::Maze, graphmaze::GraphMaze>::morph(m);
+    std::cout << Show<graphmaze::GraphMaze>::show(gm);
     std::cout << std::endl;
 
-    spelunker::maze::Maze m2 = spelunker::typeclasses::Homomorphism<spelunker::graphmaze::GraphMaze, spelunker::maze::Maze>::morph(gm);
-    std::cout << spelunker::typeclasses::Show<spelunker::maze::Maze>::show(m2);
+    spelunker::maze::Maze m2 = Homomorphism<graphmaze::GraphMaze, maze::Maze>::morph(gm);
+    std::cout << Show<maze::Maze>::show(m2);
     std::cout << std::endl;
 
-    // We also test the composition homomorphism
-    spelunker::thickmaze::ThickMaze tm =
-            spelunker::typeclasses::CompositionHomomorphism<spelunker::graphmaze::GraphMaze,
-                                                            spelunker::maze::Maze,
-                                                            spelunker::thickmaze::ThickMaze>::morph(gm);
+//    thickmaze::ThickMaze tm =
+//            CompositionMorphism<Homomorphism<graphmaze::GraphMaze, maze::Maze>,
+//                                Homomorphism<maze::Maze, thickmaze::ThickMaze>>::morph(gm);
+    auto tm = Homomorphism<graphmaze::GraphMaze, thickmaze::ThickMaze>::morph(gm);
     std::cout << spelunker::typeclasses::Show<spelunker::thickmaze::ThickMaze>::show(tm);
     return 0;
 }
