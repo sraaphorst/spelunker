d5f02bcc54c7a8e2f22be3b921e07c4a879130b0
sraaphorst <sraaphorst@gemini.edu>
2018-06-23 21:47:28 -0400

    T95: Forgot to add Travis.h

---
+++ src/Travis.h
@@ -0,0 +1,18 @@
+/**
+ * Travis.h
+ *
+ * By Sebastian Raaphorst, 2018.
+ *
+ * This is because Travis is completely incompetent and missing a number of things.
+ */
+
+#pragma once
+
+#ifdef TRAVIS
+namespace std {
+    template<typename T, typename... Args>
+    unique_ptr<T> make_unique(Args&&... args) {
+        return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+    }
+}
+#endif
