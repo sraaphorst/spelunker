8c436e5e89f5589dc17bbbd793c62553bd0048fd
sraaphorst <sraaphorst@gemini.edu>
2018-07-03 21:44:34 -0400

    T128: Making Travis happy.

--- src/squashedmaze/SquashedMaze.h
+++ src/squashedmaze/SquashedMaze.h
@@ -445,13 +445,15 @@ namespace spelunker::squashedmaze {
                     }
 
                     // If we have found v, stop.
-                    const auto &[vdist, vpath] = bfsData[v];
+                    const auto vdist = bfsData[v].distance;
+                    const auto &vpath = bfsData[v].path;
                     if (vdist != -1)
                         break;
                 }
 
                 // Make sure we have found v.
-                const auto &[vdist, vpath] = bfsData[v];
+                const auto vdist = bfsData[v].distance;
+                const auto &vpath = bfsData[v].path;
                 assert(vdist != -1);
 
                 // Insert the weighted edge and modify edges to record the path.
